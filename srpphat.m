68ba8b847f46a19cd49dd70633f9fd32f1487d4d
Julio Carabias <julio.carabias@upf.edu>
2016-12-04 23:47:42 +0100

    Update srpphat.m

--- srpphat.m
+++ srpphat.m
@@ -1,7 +1,7 @@
-function [finalpos,finalsrp]=srpphat3(y, mic_loc, fs, lsb, usb)
+function [finalpos,finalsrp]=srpphat(x, mic_loc, fs, lsb, usb)
 %% This function uses SRP-PHAT
 %% Inputs:
-%%% 1) s is "A FRAME" of data (L x M), L should be a power of 2
+%%% 1) x is "A FRAME" of data (L x M), L should be a power of 2
 %%% 2) mic_loc is the microphone 3D-locations (M x 3) ( in meters)
 %%% 3) fs: sampling rate (Hz)
 %%% 4) lsb: a row-vector of the lower rectangular search boundary, e.g., [-2 -1 0] (meters)
@@ -11,35 +11,18 @@ function [finalpos,finalsrp]=srpphat3(y, mic_loc, fs, lsb, usb)
 %%% 1) finalpos: estimated location of the source
 %%% 2) finalsrp: srp-phat value of the point source
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Usage example:
-%%% [finalpos,finalsrp,finalfe]=srplems(s, mic_loc, fs, lsb, usb)
-%%% Example of use:
-%%% 24 microphones ---> mic_loc is a (24x3) matrix.
-%%% A 2048-sample framelength ---> s is a (2048 x 24) matrix
-%%% 20 KHz sampling rate -> fs = 20,000.
-%%% A search volume of 4m x 1m x 6m with the rectangular boundaries:
-%%% lsb = [-2 -1 0] (m)
-%%% usb = [2 0 6] (m)
-%%% If the values of fs, lsb, usb are not specified in the inputs,
-%%% fs=20,000Hz, lsb = [-2 -1 0], usb = [2 0 6] will be used.
+
+ngrid = 1810;
+win = 4096;
+sp_resolution= .5;
 
 %% Initialize variables:
-
-warning off all
-
 if nargin < 5, usb=[14 22 5]; end
 if nargin < 4, lsb=[1 1 1]; end
 if nargin < 3, fs=44100; end
 
-[nsampl,nchan] = size(y);
-wlen = 4096;         %%% determine frame-length
 M = size(mic_loc,1); %%% number of microphones (also number of channels of the input X).
-np=M*(M-1)/2;        %%%number of independent pairs
-
-%dftsize = wlen*8;        %%% dft size equals to frame size
-%temperatureC=24.0;
-%speedofsound=331.4*sqrt(1.0+(temperatureC/273));
-%magiconst=fs/speedofsound;
+np=M*(M-1)/2;        %%% number of independent pairs
 
 %% Determine the maximum end-fire length (in samples) of a microphone pairs:
 mdist=pdist(mic_loc);
@@ -47,28 +30,26 @@ efmax=max(mdist);
 
 
 %% Doing the GCC-PHAT:
-% Linear transform
-X = stft_multi(y.',wlen);
-X = X(2:end,:,:);
 
-[nbin,nfram] = size(X(:,:,1));
-ngrid = 1810;
+% Calcula STFT y se queda con la fase
+X = stft_multi(x',win);
+[nbin,nfram,nchan] = size(X);
+
 tau_grid = linspace(-efmax/343,efmax/343,ngrid);
-f = fs/wlen*(1:wlen/2).';
+f = linspace(0,(fs/2),nbin)';
 
 R = zeros(ngrid,np);
 p=1;
 for ii=1:M-1
+    X1 = X(:,:,ii);
     for jj=ii+1:M,
-        X1 = X(:,:,ii);
-        X2 = X(:,:,jj);
-        
         spec = zeros(nbin,nfram,ngrid);
+    
+        X2 = X(:,:,jj);      
         P = X1.*conj(X2);
         P = P./(abs(P)+eps);
+        
         for ind = 1:ngrid,
-            % EXP = repmat(exp(-2*1i*pi*tau_grid(ind)*f),1,nfram);
-            % spec(:,:,ind) = real(P.*EXP);
             EXP = exp(-2*1i*pi*tau_grid(ind)*f);
             spec(:,:,ind) = real(bsxfun(@times,P,EXP));
         end
@@ -82,22 +63,22 @@ end;
 bstart = lsb;
 bend = usb;
 
-if bstart==bend               %%%check if the lower search-boundary is the same as the upper one, i.e., V_{search}=0
+if bstart==bend  %%%check if the lower search-boundary is the same as the upper one, i.e., V_{search}=0
     fprintf('Search volume is 0! Please expand it');
 end
 
 %%Doing SRC:
 iter = 1;
-niter=numel(bstart(1):.1:bend(1))*numel(bstart(2):.1:bend(2));
+niter=numel(bstart(1):sp_resolution:bend(1))*numel(bstart(2):sp_resolution:bend(2));
 yval = zeros(niter,1);
 position = zeros(niter,3);
-for pos_x=bstart(1):.1:bend(1),
-    for pos_y=bstart(2):.1:bend(2),
-        %for pos_z=bstart(3):.1:bend(3),
-            pos_z=2;
+for pos_x=bstart(1):sp_resolution:bend(1),
+    for pos_y=bstart(2):sp_resolution:bend(2),
+        for pos_z=bstart(3):sp_resolution:bend(3),
+            % pos_z=2;
             [yval(iter),position(iter,:)] = fe([pos_x,pos_y,pos_z],efmax,mic_loc,R);
             iter=iter+1;
-        %end;
+        end;
     end;
 end;
 
